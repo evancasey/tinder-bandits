ca08db02ebe331a41b4319574c0262bd8ce594d4
Evan Casey <evan.casey92@gmail.com>
2014-10-30 23:40:16 -0400

    calculate user features

--- src/main/scala/TinderUser.scala
+++ src/main/scala/TinderUser.scala
@@ -6,7 +6,13 @@ case class TinderUser(`_id`: String,
                  `common_like_count`: Int,
                  `common_friend_count`: Int,
                  `birth_date`: String,
-                 `name`: String)
+                 `name`: String) {
 
-
+  val userFeatures = Seq(
+    if (`bio`.nonEmpty) 1 else 0,
+    if (`common_like_count` > 0) 1 else 0,
+    if (`common_friend_count` > 0) 1 else 0,
+    if (DateTime.parse(`birth_date`).getMillis > DateTime.parse("05-05-2013").getMillis) 1 else 0
+  )
+}
 
